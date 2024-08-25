eaa0c26570201be9b5aed2917fb808a4a6601e61
Admin <shivendrawk@gmail.com>
2024-08-25 14:51:13 +0530

    search insert position brute force and optimal

--- DSA-searching.js
+++ DSA-searching.js
@@ -44,50 +44,121 @@
     //Optimal solution
 
     // function binarSearch1(arr, target) {
-    //     let n = arr.length;
-    //     if (n === 0) return -1;
+        // let n = arr.length;
+        // if (n === 0) return -1;
     
-    //     let low = 0;
-    //     let high = n - 1;
-    //     let lowerBound = n;
+        // let low = 0;
+        // let high = n - 1;
+        // let lowerBound = n;
     
-    //     while (low <= high) {
-    //         let mid = Math.floor((low + high) / 2);
+        // while (low <= high) {
+        //     let mid = Math.floor((low + high) / 2);
     
-    //         if (arr[mid] >= target) {
-    //             lowerBound = mid;
-    //             high = mid - 1;
-    //         }
-    //         else low = mid + 1;   
-    //     }
+        //     if (arr[mid] >= target) {
+        //         lowerBound = mid;
+        //         high = mid - 1;
+        //     }
+        //     else low = mid + 1;   
+        // }
     
-    //         return lowerBound;
+        //     return lowerBound;
     // }
     
     // console.log(binarSearch1([2,5,8,9,10,15,16,20], 9));
 
 //Upper Bound 
 
-    function upperBound(arr, x) {
-        let n = arr.length;
-        if (n === 0) return -1;
+    // function upperBound(arr, x) {
+    //     let n = arr.length;
+    //     if (n === 0) return -1;
 
-        let low = 0;
-        let high = n - 1;
-        let upperBound = n;
+    //     let low = 0;
+    //     let high = n - 1;
+    //     let upperBound = n;
         
+    //     while (low <= high) {
+    //         let mid = Math.floor((low + high) / 2);
+
+    //         if (arr[mid] > x) {
+    //             upperBound = mid;
+    //             high = mid - 1;
+    //         } else {
+    //             low = mid + 1
+    //         }
+    //     }
+    //     return upperBound;
+    // }
+
+    // console.log(upperBound([1, 4, 7, 8, 10], 7)) 
+    // console.log(upperBound([1, 4, 7, 8, 10], 10))
+
+//floor and ceil value simultaneously
+
+//Brute Force
+
+    // function floorCeil(arr, target) {
+    //     let n = arr.length;
+    //     if (n === 0) return -1;
+
+    //     let floor = -1, ceil = -1;
+    //     let low = 0, high = n - 1;
+
+    //     //floor largest but <= target
+    //     while (low <= high) {
+    //         let mid = Math.floor((low + high) / 2);
+    //         if (arr[mid] <= target) {
+    //             floor = arr[mid];
+    //             low = mid + 1;
+    //         } else {
+    //             high = mid - 1;
+    //         }
+    //     }
+
+    //     //Ceil smallest but >= target
+    //     low = 0;
+    //     high = n - 1;
+    //     while (low <= high) {
+    //         let mid = Math.floor((low + high) / 2);
+    //         if (arr[mid] >= target) {
+    //             ceil = arr[mid];
+    //             high = mid - 1;
+    //         } else {
+    //             low = mid + 1;
+    //         }
+    //     }
+        
+    //     return [floor, ceil];
+    // }
+
+    // console.log(floorCeil([1, 2, 8, 10, 10, 12, 19], 5))
+
+//Optimal Solution
+
+    function floorCeil(arr, target) {
+        let n = arr.length;
+        if (n === 0) return [-1 , -1];
+
+        let floor = -1, ceil = -1;
+        let low = 0, high = n - 1;
+
+    //floor largest but <= target
         while (low <= high) {
             let mid = Math.floor((low + high) / 2);
 
-            if (arr[mid] > x) {
-                upperBound = mid;
+            if (arr[mid] === target) {
+                floor = arr[mid];
+                ceil = arr[mid];
+
+                return [floor, ceil];
+            } else if (arr[mid] > target) {
+                ceil = arr[mid];
                 high = mid - 1;
             } else {
-                low = mid + 1
+                floor = arr[mid];
+                low = mid + 1;
             }
         }
-        return upperBound;
-    }
 
-    console.log(upperBound([1, 4, 7, 8, 10], 7)) 
-    console.log(upperBound([1, 4, 7, 8, 10], 10))
+        return [floor, ceil];
+    }
+    console.log(floorCeil([1, 2, 8, 10, 10, 12, 19], 5))
