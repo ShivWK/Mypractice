46e52d60bd3e85db7bd45dd808e18ff99c480d4c
Admin <shivendrawk@gmail.com>
2024-10-06 20:15:33 +0530

    find the start of the loop optimal

--- linkedList.js
+++ linkedList.js
@@ -1156,4 +1156,26 @@ function print(head) {
 
     //Optimal solution
 
-       
+    function startPointOfLoopOptimal() {
+        if (!head || !head.next) return null;
+
+        let slow = head, fast = head;
+
+        while (fast && fast.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow === fast) {
+                slow = head;
+
+                while (slow !== fast) {
+                    slow = slow.next;
+                    fast = fast.next
+                }
+
+                return slow;
+            }
+        }
+
+        return null
+    }
