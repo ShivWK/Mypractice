84157ff617ba518362c7efb32c1fc9826c56be4f
Admin <shivendrawk@gmail.com>
2024-10-18 17:58:01 +0530

    queues initial

--- stacks.js
+++ stacks.js
@@ -43,12 +43,70 @@ class Stack {
     }
 }
 
-const stack = new Stack(3);
+// const stack = new Stack(3);
 
-stack.push(11);
-stack.push(12);
-stack.push(13);
-stack.push(14);
-stack.push(15);
-stack.print();
+// stack.push(11);
+// stack.push(12);
+// stack.push(13);
+// stack.push(14);
+// stack.push(15);
+// stack.print();
+
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+
+    //in JS there is no limit to the size of an array so we dont have any restriction of size.
+
+    enqueue(element) {
+        this.queue.push(element);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return "Underflow, cannot perform dequeue.";
+        }
+
+        return this.queue.shift();
+    }
+
+    isEmpty() {
+        return this.queue.length === 0;
+    }
+
+    front() {
+        if (this.isEmpty()) {
+            return "NO element in the queue."
+        }
+
+        return this.queue[0];
+    }
+
+    size() {
+        return this.queue.length;
+    }
+
+    print() {
+        let result = '';
+
+        for (let i = 0; i < this.size(); i++) {
+            result += i === this.size() - 1 ? this.queue[i] : this.queue[i] + ", ";
+        }
+
+        return result;
+    }
+}
+
+let queue = new Queue();
+
+// queue.enqueue(11);
+// queue.enqueue(12);
+// queue.enqueue(13);
+// queue.enqueue(14);
+// queue.enqueue(15);
+
+console.log(queue.print());
+console.log(queue.dequeue());
+console.log(queue.front());
 
