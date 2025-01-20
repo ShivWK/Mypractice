14ece5cf0a84514486d33e725eba613a507c6a5e
Admin <shivendrawk@gmail.com>
2025-01-20 15:06:49 +0530

    new formated

--- stacks.js
+++ stacks.js
@@ -1,145 +1,144 @@
-class Stack {
-    constructor(maxsize = null) {
-        this.stack = [];
-        this.maxsize = maxsize;
-    }
+//Stack useing arrays
 
-    push(eleemnt) {
-        if (this.size() === this.maxsize) {
-            console.error("Stack Overflow: Cannot add more elements.");
-        } else {
-            this.stack.push(eleemnt);
+        class Stack {
+            constructor(maxsize = null) {
+                this.stack = [];
+                this.maxsize = maxsize;
+            }
+
+            push(eleemnt) {
+                if (this.size() === this.maxsize) {
+                    console.error("Stack Overflow: Cannot add more elements.");
+                } else {
+                    this.stack.push(eleemnt);
+                }
+            }
+
+            pop() {
+                if (this.size() === 0) {
+                    return "stack is empty";
+                }
+
+                return this.stack.pop();
+            }
+
+            peek() {
+                if (this.size() === 0) {
+                    return "stack is empty";
+                }
+
+                return this.stack[this.stack.length - 1];
+            }
+
+            isEmpty() {
+                return this.stack.length === 0;
+            }
+
+            size() {
+                return this.stack.length;
+            }
+
+            print() {
+                for (let i = 0; i < this.size(); i++) {
+                    console.log(i === this.size() - 1 ? this.stack[i] + '' : this.stack[i] + ",");
+                }
+            }
         }
-    }
 
-    pop() {
-        if (this.size() === 0) {
-            return "stack is empty";
+        // const stack = new Stack(3);
+        // stack.push(11);
+        // stack.push(12);
+        // stack.push(13);
+        // stack.push(14);
+        // stack.push(15);
+        // stack.print();
+
+//Queue using arrays
+
+        class Queue {
+            constructor() {
+                this.queue = [];
+            }
+
+            enqueue(element) {
+                this.queue.push(element);
+            }
+
+            dequeue() {
+                if (this.isEmpty()) {
+                    return "Underflow, cannot perform dequeue.";
+                }
+
+                return this.queue.shift();
+            }
+
+            isEmpty() {
+                return this.queue.length === 0;
+            }
+
+            front() {
+                if (this.isEmpty()) {
+                    return "NO element in the queue."
+                }
+
+                return this.queue[0];
+            }
+
+            size() {
+                return this.queue.length;
+            }
+
+            print() {
+                let result = '';
+
+                for (let i = 0; i < this.size(); i++) {
+                    result += i === this.size() - 1 ? this.queue[i] : this.queue[i] + ", ";
+                }
+
+                return result;
+            }
         }
 
-        return this.stack.pop();
-    }
+        // let queue = new Queue();
+        // queue.enqueue(11);
+        // queue.enqueue(12);
+        // queue.enqueue(13);
+        // queue.enqueue(14);
+        // queue.enqueue(15);
+        // console.log(queue.print());
+        // console.log(queue.dequeue());
+        // console.log(queue.front());
 
-    peek() {
-        if (this.size() === 0) {
-            return "stack is empty";
-        }
+//Balanced Prarenthesis
 
-        return this.stack[this.stack.length - 1];
-    }
+        function balancedParenthesis(str) {
+            let stack = new Stack()
 
-    isEmpty() {
-        return this.stack.length === 0;
-    }
+            for (let i = 0; i < str.length; i++) {
+                if (str[i] === "(" || str[i] === "{" || str[i] === "[") {
 
-    size() {
-        return this.stack.length;
-    }
+                    stack.push(str[i]);
+                } else {
+                    let popped = stack.pop();
 
-    print() {
-        for (let i = 0; i < this.size(); i++) {
-            console.log(i === this.size() - 1 ? this.stack[i] + '' : this.stack[i] + ",");
-        }
-    }
-}
+                    if (popped === "stack is empty") {
+                        return false;
+                    }
 
-// const stack = new Stack(3);
-
-// stack.push(11);
-// stack.push(12);
-// stack.push(13);
-// stack.push(14);
-// stack.push(15);
-// stack.print();
-
-// class Queue {
-//     constructor() {
-//         this.queue = [];
-//     }
-
-//     //in JS there is no limit to the size of an array so we dont have any restriction of size.
-
-//     enqueue(element) {
-//         this.queue.push(element);
-//     }
-
-//     dequeue() {
-//         if (this.isEmpty()) {
-//             return "Underflow, cannot perform dequeue.";
-//         }
-
-//         return this.queue.shift();
-//     }
-
-//     isEmpty() {
-//         return this.queue.length === 0;
-//     }
-
-//     front() {
-//         if (this.isEmpty()) {
-//             return "NO element in the queue."
-//         }
-
-//         return this.queue[0];
-//     }
-
-//     size() {
-//         return this.queue.length;
-//     }
-
-//     print() {
-//         let result = '';
-
-//         for (let i = 0; i < this.size(); i++) {
-//             result += i === this.size() - 1 ? this.queue[i] : this.queue[i] + ", ";
-//         }
-
-//         return result;
-//     }
-// }
-
-// let queue = new Queue();
-
-// queue.enqueue(11);
-// queue.enqueue(12);
-// queue.enqueue(13);
-// queue.enqueue(14);
-// queue.enqueue(15);
-
-// console.log(queue.print());
-// console.log(queue.dequeue());
-// console.log(queue.front());
-
-//Balanced String
-
-        // function balancedParenthesis(str) {
-        //     let stack = new Stack()
-
-        //     for (let i = 0; i < str.length; i++) {
-        //         if (str[i] === "(" || str[i] === "{" || str[i] === "[") {
-
-        //             stack.push(str[i]);
-        //         } else {
-        //             let popped = stack.pop();
-
-        //             if (popped === "stack is empty") {
-        //                 return false;
-        //             }
-
-        //             if (
-        //                 (str[i] === ")" && popped !== "(") || 
-        //                 (str[i] === "}" && popped !== "{") || 
-        //                 (str[i] === "]" && popped !== "[")
-        //                 ) 
-        //                 {
-        //                 return false;
-        //             } 
+                    if (
+                        (str[i] === ")" && popped !== "(") || 
+                        (str[i] === "}" && popped !== "{") || 
+                        (str[i] === "]" && popped !== "[")
+                        ) 
+                        {
+                        return false;
+                    } 
                     
-        //         }
-        //     }
+                }
+            }
 
-        //     return stack.isEmpty();
-        // }
+            return stack.isEmpty();
+        }
 
         // console.log(balancedParenthesis("(){[()]}"));
         // console.log(balancedParenthesis("]["));
@@ -155,105 +154,108 @@ class Stack {
 
     //infix to postfix conversion
     
-    function infixToPostfix(str) {
-        let ans = '', i = 0;
-        let stack1 = new Stack();
-        let n = str.length;
+        function infixToPostfix(str) {
+            let ans = '', i = 0;
+            let stack1 = new Stack();
+            let n = str.length;
 
-        while (i < n) {
-            if (str[i] >= "A" && str[i] <= "Z" || str[i] >= "a" && str[i] <= 'z' || str[i] >= '0' && str[i] <= '9') {
-                ans += str[i];
+            while (i < n) {
+                if (str[i] >= "A" && str[i] <= "Z" || str[i] >= "a" && str[i] <= 'z' || str[i] >= '0' && str[i] <= '9') {
+                    ans += str[i];
 
-            } else if (str[i] == '(' ) {
-                stack1.push(str[i]);
+                } else if (str[i] == '(' ) {
+                    stack1.push(str[i]);
 
-            } else if (str[i] == ')') {
-                while (!stack1.isEmpty() && stack1.peek() !== "(") {
-                    ans += stack1.pop();
+                } else if (str[i] == ')') {
+                    while (!stack1.isEmpty() && stack1.peek() !== "(") {
+                        ans += stack1.pop();
 
-                }
-                stack1.pop();
-            } else {
-                while (!stack1.isEmpty() && priority(stack1.peek()) >= priority(str[i])) {
-                    ans += stack1.pop();
-
-                }
-                stack1.push(str[i]);
-            }
-
-            i++;
-        }
-
-        while (!stack1.isEmpty()) {
-            ans += stack1.pop();
-        }
-
-        return ans;
-    }
-
-    // console.log(convertion("a+b*(c^d-c)"));
-
-    function reverse(str) {
-        let ans = '';
-
-        for (let i = str.length - 1; i >= 0; i--) {
-            if (str[i] === "(") {
-                ans += ")";
-            } else if (str[i] === ")") {
-                ans += "(";
-            } else {
-                ans += str[i];
-            }
-        }
-
-        return ans;
-    }
-
-    function infixToPrefix(str) {
-        let ans = '', i = 0;
-        let stack1 = new Stack();
-        let n = str.length;
-
-        str = reverse(str);
-
-        while (i < n) {
-            if (str[i] >= "A" && str[i] <= "Z" || str[i] >= "a" && str[i] <= 'z' || str[i] >= '0' && str[i] <= '9') {
-                ans += str[i];
-
-            } else if (str[i] == '(' ) {
-                stack1.push(str[i]);
-
-            } else if (str[i] == ')') {
-                while (!stack1.isEmpty() && stack1.peek() !== "(") {
-                    ans += stack1.pop();
-
-                }
-                stack1.pop(); //poping the opening bracket too.
-            } else {
-                if (str[i] === '^') {
+                    }
+                    stack1.pop();
+                } else {
                     while (!stack1.isEmpty() && priority(stack1.peek()) >= priority(str[i])) {
                         ans += stack1.pop();
-                    }
 
-                } else {
-                    while (!stack1.isEmpty() && priority(stack1.peek()) > priority(str[i])) {
-                        ans += stack1.pop();
-    
                     }
+                    stack1.push(str[i]);
                 }
-                
-                stack1.push(str[i]);
+
+                i++;
             }
 
-            i++;
+            while (!stack1.isEmpty()) {
+                ans += stack1.pop();
+            }
+
+            return ans;
         }
 
-        while (!stack1.isEmpty()) {
-            ans += stack1.pop();
+        // console.log(convertion("a+b*(c^d-c)"));
+
+        function reverse(str) {
+            let ans = '';
+
+            for (let i = str.length - 1; i >= 0; i--) {
+                if (str[i] === "(") {
+                    ans += ")";
+                } else if (str[i] === ")") {
+                    ans += "(";
+                } else {
+                    ans += str[i];
+                }
+            }
+
+            return ans;
         }
-        ans = reverse(ans);
 
-        return ans;
-    }
+    // infix to prefix
 
-    console.log(infixToPrefix('(A*B)+(C*D)'));
+        function infixToPrefix(str) {
+            let ans = '', i = 0;
+            let stack1 = new Stack();
+            let n = str.length;
+
+            str = reverse(str);
+
+            while (i < n) {
+                if (str[i] >= "A" && str[i] <= "Z" || str[i] >= "a" && str[i] <= 'z' || str[i] >= '0' && str[i] <= '9') {
+                    ans += str[i];
+
+                } else if (str[i] == '(' ) {
+                    stack1.push(str[i]);
+
+                } else if (str[i] == ')') {
+                    while (!stack1.isEmpty() && stack1.peek() !== "(") {
+                        ans += stack1.pop();
+
+                    }
+                    stack1.pop(); //poping the opening bracket too.
+                } else {
+                    if (str[i] === '^') {
+                        while (!stack1.isEmpty() && priority(stack1.peek()) >= priority(str[i])) {
+                            ans += stack1.pop();
+                        }
+
+                    } else {
+                        while (!stack1.isEmpty() && priority(stack1.peek()) > priority(str[i])) {
+                            ans += stack1.pop();
+        
+                        }
+                    }
+                    
+                    stack1.push(str[i]);
+                }
+
+                i++;
+            }
+
+            while (!stack1.isEmpty()) {
+                ans += stack1.pop();
+            }
+            ans = reverse(ans);
+
+            return ans;
+        }
+
+        console.log(infixToPrefix('(A*B)+(C*D)'));
+
