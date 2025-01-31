1e721b98059cc0ef6c80960422cff9e6946b3efd
Admin <shivendrawk@gmail.com>
2025-01-31 22:26:18 +0530

    next greater element second part brute force solution

--- stacks.js
+++ stacks.js
@@ -863,7 +863,7 @@
             return ansArray;
         }
 
-        console.log(NgeBrute([6,8,0,8,1,3]))
+        // console.log(NgeBrute([6,8,0,8,1,3]))
 
     //Optimal solution
 
@@ -899,7 +899,7 @@
             return ansArray;
         }
 
-        console.log(NgeOptimal([6,8,0,8,1,3]))
+        // console.log(NgeOptimal([2,4,3,1,8,9,7,2,5,1,6,5]))
 
         //more shorter 
 
@@ -920,10 +920,44 @@
             return ansArray;
         }
         
-        console.log(NgeOptimal([6, 0, 8, 1, 3])); // Output: [8, 8, -1, 3, -1]
+        // console.log(NgeOptimal([2,4,3,1,8,9,7,2,5,1,6,5]));
+
+//Next greter element 2 (Circular array)
+
+    //Brute force
         
+        function FNGE1(arr) {
+            if (arr.length === 0) return "Array/Stack is empty!";
+            let size = arr.length;
+            let ansArray = [];
 
+            for (let i = 0; i <= size - 1; i++) {
+                for (let j = i + 1; j <= size - 1; j++) {
+                    if (arr[i] < arr[j]) {
+                        ansArray[i] = arr[j];
+                        break
+                    }
+                }
+                if (ansArray[i] == undefined) {
+                    for (let j = 0; j <= i - 1; j++) {
+                        if (arr[i] < arr[j]) {
+                            ansArray[i] = arr[j];
+                            break
+                        }
+                    }
+                }
 
+                if (ansArray[i] == undefined) ansArray[i] = -1;
+            }
+
+            return ansArray;
+        }
+
+        console.log(FNGE1([2,10,12,1,11]))
+        
+    //Better solution
+
+       
 
 
 
