8471bd253c2697ef49c5496cb9929c23bad4c00b
Admin <shivendrawk@gmail.com>
2025-02-18 16:45:37 +0530

    flater funcion

--- practice.js
+++ practice.js
@@ -13,4 +13,37 @@ function spliptIt(arr) {
     return ansArray;
 }
 
-console.log(spliptIt([{name: "banana" , quantity: 2}, {name: "carrat" , quantity: 3}]))
+// console.log(spliptIt([{name: "banana" , quantity: 2}, {name: "carrat" , quantity: 3}]))
+
+//flatening nested arrays
+
+   function flarter(arr) {
+      let answer = [];
+
+      for (let customers of arr) {
+         let {username, orders} = customers;
+
+         for (let order of orders) {
+            answer.push({
+               username,
+               ...order
+            })
+         }
+      }
+
+      return answer;
+   }
+
+   let customers = [
+      {
+         username : "Shivendra",
+         orders : [
+            {item : "perse", amounr : 1500},
+            {item : "phone", amounr : 20000},
+            {item : "charger", amounr : 800},
+            {item : "cover", amounr : 4000},
+         ]
+      },
+   ]
+
+   console.log(flarter(customers));
