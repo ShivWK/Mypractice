fc5bad3c69da67cec0465ac27955349330cf8158
Admin <shivendrawk@gmail.com>
2025-03-16 03:32:05 +0530

    finding the missing number in an array

--- practice.js
+++ practice.js
@@ -108,8 +108,97 @@ count.reset = function() {
 
    let multipyBy2 = multiplyer(2);
 
-   console.log(multipyBy2(4))
-   console.log(multipyBy2(5))
-   console.log(multipyBy2(6))
-   console.log(multipyBy2(7))
+   // console.log(multipyBy2(4))
+   // console.log(multipyBy2(5))
+   // console.log(multipyBy2(6))
+   // console.log(multipyBy2(7))
+
+// Value of 'this' practice
+
+        // Inside a regular function non strict mode
+
+          // in global
+
+         //  function inGlobal() {
+         //    console.log(this);
+         //  }
+
+         //  inGlobal();
+
+      //   // Inside an object
+      //     let name = "Outside"
+
+      //     let obj1 = {
+      //       name : "Inside",
+      //       show : function() {
+      //         console.log(this);
+      //       }
+      //     }
+
+      //     obj1.show()
+
+      //   // Inside nested function
+
+      //     function first() {
+      //       function second() {
+      //         console.log(this);
+      //       }
+
+      //       second()
+      //     }
+
+      //     // first.name = "Inside first function";
+
+      //     first()
+
+      //     let obj2 = {
+      //       name : "Inside",
+      //       first : function() {
+      //         function second() {
+      //           console.log(this);
+      //         }
+
+      //         second();
+      //       }
+      //     }
+
+      //     obj2.first();
+
+// Find the missing number in the array of consecutive integers
+
+   //Works only for increasing arrays
+   function findMissingNum(arr) {
+      let firstIndexNum = arr[0];
+      firstIndexNum++;
+      let arrlength = arr.length;
+
+      for (let i = 1; i < arrlength; i++) {
+         if (arr[i] !== firstIndexNum) return firstIndexNum; 
+
+         firstIndexNum++;
+      }
+
+      return null;
+   }
+
+   console.log(findMissingNum([2,3,4,5,6,7])); //null
+   console.log(findMissingNum([2,4,5,6,7])); //3
+
+   //Works for both increasing and decreasing arrays
+   function findMissingNum(arr) {
+      let isAscending = arr[0] < arr[arr.length - 1]; 
+      let expectedNum = arr[0] + (isAscending ? 1 : -1); 
+  
+      for (let i = 1; i < arr.length; i++) {
+          if (arr[i] !== expectedNum) return expectedNum;
+          expectedNum += isAscending ? 1 : -1;
+      }
+  
+      return null; 
+  }
+  
+  console.log(findMissingNum([2, 3, 4, 5, 6, 7])); // null (no missing number)
+  console.log(findMissingNum([2, 4, 5, 6, 7])); // 3 (increasing order)
+  console.log(findMissingNum([7, 6, 4, 3, 2])); // 5 (decreasing order)
+  
 
